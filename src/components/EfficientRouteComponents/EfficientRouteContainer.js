561193779667f0ad802ad684b1954f88706b7784
Michael Kim <m.kim4247@gmail.com>
2020-07-07 12:37:55 -0400

    find most efficient helper methods working

--- src/components/EfficientRouteComponents/EfficientRouteContainer.js
+++ src/components/EfficientRouteComponents/EfficientRouteContainer.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import EfficientRouteInfo from './EfficientRouteInfo'
+import { findMostEfficientRoute, countries } from './helperMethods'
 
 const EfficientRouteContainer = props => {
+  console.log(findMostEfficientRoute(countries))
 
   return(
     <div>
--- src/components/EfficientRouteComponents/helperMethods.js
+++ src/components/EfficientRouteComponents/helperMethods.js
@@ -3,35 +3,7 @@ import { getDistance } from 'geolib';
 // accuracy default is 1m
 // start and end are objs w lat/long keys, ex: {latitude: 52.518611, longitude: 13.408056}
 
-  let countries = [
-    {
-      "id": "ABW",
-      "iso2Code": "AW",
-      "name": "Aruba",
-      "region": {
-        "id": "LCN",
-        "iso2code": "ZJ",
-        "value": "Latin America & Caribbean "
-      },
-      "adminregion": {
-        "id": "",
-        "iso2code": "",
-        "value": ""
-      },
-      "incomeLevel": {
-        "id": "HIC",
-        "iso2code": "XD",
-        "value": "High income"
-      },
-      "lendingType": {
-        "id": "LNX",
-        "iso2code": "XX",
-        "value": "Not classified"
-      },
-      "capitalCity": "Oranjestad",
-      "longitude": "-70.0167",
-      "latitude": "12.5167"
-    },
+  export const countries = [
     {
       "id": "AFG",
       "iso2Code": "AF",
@@ -59,6 +31,34 @@ import { getDistance } from 'geolib';
       "capitalCity": "Kabul",
       "longitude": "69.1761",
       "latitude": "34.5228"
+    },
+    {
+      "id": "ABW",
+      "iso2Code": "AW",
+      "name": "Aruba",
+      "region": {
+        "id": "LCN",
+        "iso2code": "ZJ",
+        "value": "Latin America & Caribbean "
+      },
+      "adminregion": {
+        "id": "",
+        "iso2code": "",
+        "value": ""
+      },
+      "incomeLevel": {
+        "id": "HIC",
+        "iso2code": "XD",
+        "value": "High income"
+      },
+      "lendingType": {
+        "id": "LNX",
+        "iso2code": "XX",
+        "value": "Not classified"
+      },
+      "capitalCity": "Oranjestad",
+      "longitude": "-70.0167",
+      "latitude": "12.5167"
     },
     {
       "id": "AGO",
@@ -118,29 +118,7 @@ import { getDistance } from 'geolib';
     }
   ]
 
-export const findEfficientRoute = cities => {
-  let city1 = cities[0];
-  let city2 = cities[1];
-  let city3 = cities[2];
-  let city4 = cities[3];
-
-  let longestRoute;
-
-  for(let i = 0; i < cities.length; i++){
-    for(let j = i + 1; j < cities.length; j++){
-      let distance = findDistanceBtwnCities(cities[i], cities[j]);
-
-      if(distance.distance > longestRoute){
-        longestRoute = distance
-      }
-    }
-  }
-}
-
-const findDistanceBtwnCities = cities => {
-  let city1 = cities[0];
-  let city2 = cities[1];
-
+const findDistanceBtwnCities = (city1, city2) => {
   let coords1 = {
     latitude: city1['latitude'],
     longitude: city1['longitude']
@@ -153,9 +131,77 @@ const findDistanceBtwnCities = cities => {
 
   let distance = getDistance(coords1, coords2);
 
+  return distance;
+}
+
+const findLongestRoute = cities => {
+  let longestRoute = {
+    distance: 0,
+    start: null,
+    end: null
+  };
+
+  for(let i = 0; i < cities.length; i++){
+    for(let j = i + 1; j < cities.length; j++){
+      let distance = findDistanceBtwnCities(cities[i], cities[j]);
+
+      if(distance > longestRoute.distance){
+        longestRoute.distance = distance
+        longestRoute.start = cities[i]
+        longestRoute.end = cities[i + 1]
+      }
+    }
+  }
+
+  return longestRoute;
+}
+
+// little hard-codey rn => look to improve later
+const findPossibleRoutes = (cities, start, end) => {
+  let stops = [];
+
+  for(let i = 0; i < cities.length; i++){
+    if(cities[i].name !== start.name && cities[i].name !== end.name){
+      stops.push(cities[i])
+    }
+  }
+
+  let city1 = stops[0];
+  let city2 = stops[1];
+
+  let route1 = [start, city1, city2, end];
+  let route2 = [start, city2, city1, end];
+
+  return [route1, route2];
+}
+
+const findTotalRouteDistance = route => {
+  let total = 0;
+
+  for(let i = 0; i < route.length - 1; i++){
+    let distance = findDistanceBtwnCities(route[i], route[i + 1])
+
+    total += distance
+  }
+
+  return total
+}
+
+const findShortestRoute = routes => {
+  return routes.reduce( (a, b) => {
+    return findTotalRouteDistance(a) < findTotalRouteDistance(b) ? a : b
+  })
+}
+
+export const findMostEfficientRoute = cities => {
+  let longestRoute = findLongestRoute(cities);
+
+  let routes = findPossibleRoutes(cities, longestRoute.start, longestRoute.end);
+
+  let shortestRoute = findShortestRoute(routes);
+
   return {
-    start: city1,
-    end: city2,
-    distance: distance
+    route: shortestRoute,
+    distance: findTotalRouteDistance(shortestRoute)
   }
 }
